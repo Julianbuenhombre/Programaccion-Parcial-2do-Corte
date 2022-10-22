3b09322299492aa270e8481d082a8be45371f99f
Julianbuenhombre <104658843+Julianbuenhombre@users.noreply.github.com>
2022-10-21 20:07:22 -0500

    Add files via upload

---
+++ Tienda_Cachivaches_Flutter/lib/data/data.dart
@@ -0,0 +1,45 @@
+var carrousellPrincipal = [
+  {
+    'image': 'assets/images/vanilla.jpg',
+    'name': 'Muñeca de Hawaii',
+    'price': '\$19.00',
+  },
+  {
+    'image': 'assets/images/strawberry.jpeg',
+    'name': 'Chanclas de Pan',
+    'price': '\$30.00'
+  },
+  {
+    'image': 'assets/images/coffee.jpeg',
+    'name': 'Juguete de Obama',
+    'price': '\$124.00',
+  },
+];
+
+var gridPopular = [
+  {
+    'image': 'assets/images/mango.jpeg',
+    'name': 'Ropa para mascota',
+    'description': 'Favorito'
+  },
+  {
+    'image': 'assets/images/chocolate.jpeg',
+    'name': 'Tapete de Golf',
+    'description': 'Favorito'
+  },
+  {
+    'image': 'assets/images/bubblegum.jpeg',
+    'name': 'Bubble Gum',
+    'description': 'Favorito'
+  },
+  {
+    'image': 'assets/images/coffee.jpeg',
+    'name': 'Coffee',
+    'description': 'Favorito'
+  },
+  {
+    'image': 'assets/images/pistachio.jpeg',
+    'name': 'Pistachio',
+    'description': 'Favorito'
+  },
+];
